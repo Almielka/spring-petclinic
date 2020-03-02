ff668c0b9f0d5c1984f5d35270dcd972e7bea5a5
Almielka <D:\The Bat!\Data\Mail>
2020-03-02 17:36:06 +0200

    Changed schema.sql and data.sql (hsqldb and mysql)

--- src/main/resources/db/mysql/schema.sql
+++ src/main/resources/db/mysql/schema.sql
@@ -1,55 +1,56 @@
-CREATE TABLE IF NOT EXISTS vets (
+CREATE TABLE IF NOT EXISTS vet (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   INDEX(last_name)
 ) engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS specialties (
+CREATE TABLE IF NOT EXISTS speciality (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(80),
   INDEX(name)
 ) engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS vet_specialties (
+CREATE TABLE IF NOT EXISTS vet_speciality (
   vet_id INT(4) UNSIGNED NOT NULL,
   specialty_id INT(4) UNSIGNED NOT NULL,
-  FOREIGN KEY (vet_id) REFERENCES vets(id),
-  FOREIGN KEY (specialty_id) REFERENCES specialties(id),
+  FOREIGN KEY (vet_id) REFERENCES vet(id),
+  FOREIGN KEY (specialty_id) REFERENCES speciality(id),
   UNIQUE (vet_id,specialty_id)
 ) engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS types (
+CREATE TABLE IF NOT EXISTS pet_type (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(80),
   INDEX(name)
 ) engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS owners (
+CREATE TABLE IF NOT EXISTS owner (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   address VARCHAR(255),
   city VARCHAR(80),
-  telephone VARCHAR(20),
+  created_at DATE,
+  phone VARCHAR(20),
   INDEX(last_name)
 ) engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS pets (
+CREATE TABLE IF NOT EXISTS pet (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30),
-  birth_date DATE,
-  type_id INT(4) UNSIGNED NOT NULL,
+  birthday_date DATE,
   owner_id INT(4) UNSIGNED NOT NULL,
+  pet_type_id INT(4) UNSIGNED NOT NULL,
   INDEX(name),
-  FOREIGN KEY (owner_id) REFERENCES owners(id),
-  FOREIGN KEY (type_id) REFERENCES types(id)
+  FOREIGN KEY (owner_id) REFERENCES owner(id),
+  FOREIGN KEY (pet_type_id) REFERENCES pet_type(id)
 ) engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS visits (
+CREATE TABLE IF NOT EXISTS visit (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  pet_id INT(4) UNSIGNED NOT NULL,
   visit_date DATE,
   description VARCHAR(255),
-  FOREIGN KEY (pet_id) REFERENCES pets(id)
+  pet_id INT(4) UNSIGNED NOT NULL,
+  FOREIGN KEY (pet_id) REFERENCES pet(id)
 ) engine=InnoDB;
